74541b8d1deeab41f97b54c3d1498dcb5a60742e
RubiiW <robin.weder@protonmail.com>
2026-05-26 20:52:42 +0200

    Init

--- src/main/java/net/triblock/bng/item/ModItems.java
+++ src/main/java/net/triblock/bng/item/ModItems.java
@@ -10,21 +10,9 @@ import net.triblock.bng.BoltsNGears;
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(BoltsNGears.MODID);
 
-    public static final DeferredItem<ArmorItem> HAZMAT_HOOD = ITEMS.register("hazmat_hood",
-            () -> new ArmorItem(ModArmorMaterials.HAZMAT_ARMOR_MATERIAL, ArmorItem.Type.HELMET,
-                    new Item.Properties().durability(ArmorItem.Type.HELMET.getDurability(20))));
+    public static final DeferredItem<Item> ITEM_VARIABLE_NAME = ITEMS.register("item_id",
+            () -> new Item(new Item.Properties()));
 
-    public static final DeferredItem<ArmorItem> HAZMAT_SUIT = ITEMS.register("hazmat_suit",
-            () -> new ArmorItem(ModArmorMaterials.HAZMAT_ARMOR_MATERIAL, ArmorItem.Type.CHESTPLATE,
-                    new Item.Properties().durability(ArmorItem.Type.CHESTPLATE.getDurability(20))));
-
-    public static final DeferredItem<ArmorItem> HAZMAT_PANTS = ITEMS.register("hazmat_pants",
-            () -> new ArmorItem(ModArmorMaterials.HAZMAT_ARMOR_MATERIAL, ArmorItem.Type.LEGGINGS,
-                    new Item.Properties().durability(ArmorItem.Type.LEGGINGS.getDurability(20))));
-
-    public static final DeferredItem<ArmorItem> HAZMAT_BOOTS = ITEMS.register("hazmat_boots",
-            () -> new ArmorItem(ModArmorMaterials.HAZMAT_ARMOR_MATERIAL, ArmorItem.Type.BOOTS,
-                    new Item.Properties().durability(ArmorItem.Type.BOOTS.getDurability(20))));
 
     public static void register(IEventBus bus) {
         ITEMS.register(bus);
