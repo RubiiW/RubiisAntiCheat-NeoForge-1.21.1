36f6bc2537e2f7d06f32107a01f3c77cef7f65a8
RubiiW <robin.weder@protonmail.com>
2026-08-14 15:01:27 +0200

    Tweaked English Translation. Tweaked Brightness check. Changed periodic monitoring system and cave light multiplier checking system

--- src/main/java/net/rubii/rac/EventHandler.java
+++ src/main/java/net/rubii/rac/EventHandler.java
@@ -4,16 +4,61 @@ import net.minecraft.server.level.ServerPlayer;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.neoforged.neoforge.event.tick.ServerTickEvent;
 import net.neoforged.neoforge.network.PacketDistributor;
 import net.rubii.rac.network.payload.GraphicsSettingsRequestPayload;
+import net.rubii.rac.network.payload.ModFilesLoggingRequestPayload;
+import net.rubii.rac.network.payload.ModsIntegrityRequestPayload;
+import net.rubii.rac.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EventBusSubscriber
 public class EventHandler {
 
+    private static int ticks = 0;
+
     @SubscribeEvent
     public static void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
         if (event.getEntity() instanceof ServerPlayer player) {
-            PacketDistributor.sendToPlayer(player, new GraphicsSettingsRequestPayload(true));
+            List<String> values = new ArrayList<>();
+
+            for (Integer value : Config.CAVE_LIGHT_MULTIPLIER_VALUES.get()){
+                values.add(value.toString());
+            }
+
+            PacketDistributor.sendToPlayer(player, new GraphicsSettingsRequestPayload(
+                    Utils.encodeList(Config.CAVE_LIGHT_MULTIPLIER_NAMES.get()),
+                    Utils.encodeList(values),
+                    true
+            ));
+
+            PacketDistributor.sendToPlayer(player, new ModFilesLoggingRequestPayload(true));
+            PacketDistributor.sendToPlayer(player, new ModsIntegrityRequestPayload(true));
+        }
+    }
+
+    @SubscribeEvent
+    public static void onServerTick(ServerTickEvent.Post event) {
+        for (ServerPlayer player : event.getServer().getPlayerList().getPlayers()) {
+            if (!Config.ENABLE_PERIODIC_MONITORING.get()) return;
+
+            ticks++;
+
+            if (ticks % Config.PERIODIC_MONITORING_INTERVAL.get() == 0) {
+                List<String> values = new ArrayList<>();
+
+                for (Integer value : Config.CAVE_LIGHT_MULTIPLIER_VALUES.get()){
+                    values.add(value.toString());
+                }
+
+                PacketDistributor.sendToPlayer(player, new GraphicsSettingsRequestPayload(
+                        Utils.encodeList(Config.CAVE_LIGHT_MULTIPLIER_NAMES.get()),
+                        Utils.encodeList(values),
+                        true
+                ));
+            }
         }
     }
 }
